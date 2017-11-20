39f409d90f873ccd0845ecc32fefa3f303c95d8e
Pikachu1980 <dark653855@gmail>
2017-11-20 13:53:36 +0800

    第3章的模組分開安裝不影響，但因為他有引用mail會額外安裝官方模組

---
+++ todo_user/__manifest__.py
@@ -0,0 +1,11 @@
+
+{
+    'name': 'Multiuser To-Do',
+    'description': 'Extend the To-Do app to mutiuser.',
+    'author': 'Daniel Reis',
+    'depends': ['todo_app', 'mail'],
+
+    'data': [
+        'views/todo_task.xml',
+        'security/todo_access_rules.xml'],
+}
